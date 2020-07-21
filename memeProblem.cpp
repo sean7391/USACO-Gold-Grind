3790e3aef6d93cbe1ea28a7660714bf94427afbe
sean7391 <47064367+sean7391@users.noreply.github.com>
2020-07-21 16:09:05 -0400

    Create memeProblem.cpp

---
+++ memeProblem.cpp
@@ -0,0 +1,16 @@
+// Yet Another Meme Problem: https://codeforces.com/problemset/problem/1288/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+typedef long long ll;
+
+
+int main(){
+    ll t; cin >> t;
+    while (t--){
+        ll a, b, cnt = 0; cin >> a >> b;
+        cout << a * (ll)(floor(log10(b + 1) + 1) - 1) << endl;
+    }
+}
+
