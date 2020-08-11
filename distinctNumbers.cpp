a3a26294d97a281ebb2e0a8d2d400e7b5f58edb8
Sean C <47064367+sean7391@users.noreply.github.com>
2020-08-11 11:53:17 -0400

    Update distinctNumbers.cpp

--- distinctNumbers.cpp
+++ distinctNumbers.cpp
@@ -1,20 +1,41 @@
 // Distinct Numbers: https://cses.fi/problemset/task/1621/
 
-#include <iostream>
-#include <fstream>
-#include <unordered_set>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+ 
 using namespace std;
-
+ 
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define trav(a, x) for (auto& a: x)
+#define ins insert
+#define pq priority_queue
+#define all(v) v.begin(), v.end()
+#define sz(x) (int)x.size()
+#define endl "\n"
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii; 
+ 
+const int dx[4]= {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+ 
 int main(){
-    unordered_set<int> s;
     int n; cin >> n;
-    for (int i = 0; i < n; i++){
-        int a; cin >> a;
-        s.insert(a);
-    }
-    
-        
-    cout << s.size();
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    	cin >> v[i];
+ 
+    sort(begin(v), end(v));
+    int ans = 1;
+    for (int i = 1; i < n; i++)
+    	if (v[i] != v[i - 1])
+    		ans++;
+ 
+	cout << ans << endl;
+	return 0;
 }
