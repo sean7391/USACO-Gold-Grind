0842d8d5449300ef903ecce0003d2effc6fc4613
Sean C <47064367+sean7391@users.noreply.github.com>
2020-08-16 21:53:56 -0400

    Create repetitions.cpp

---
+++ repetitions.cpp
@@ -0,0 +1,50 @@
+// Repetitions: https://cses.fi/problemset/task/1069/
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define trav(a, x) for (auto& a: x)
+#define ins insert
+#define pq priority_queue
+#define all(v) v.begin(), v.end()
+#define sz(x) (int)x.size()
+#define endl "\n"
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii; 
+ 
+const int dx[4]= {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+ 
+void setIO(string name){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	freopen((name + ".in").c_str(), "r", stdin);
+	freopen((name + ".out").c_str(), "w", stdout);
+}
+ 
+int main() {
+    // setIO("name"); 
+    string s; cin >> s;
+    char c = 'X';
+    int ans = 0, cur = 1;
+    for (int i = 0; i < sz(s); i++){
+    	if (s[i] == c)
+    		cur++;
+    	else{
+    		cur = 1;
+    		c = s[i];
+    	}
+    	
+    	ans = max(ans, cur);
+    }
+    
+    cout << ans << endl;
+}
